142dcd63f79e3bdf9657e4b5820cca6b3a1ac01d
yujeonghyeon <dbdb1114@naver.com>
2024-03-26 17:13:05 +0900

    프로젝트 생성

---
+++ src/test/java/fems/loadtest/LoadtestApplicationTests.java
@@ -0,0 +1,13 @@
+package fems.loadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
